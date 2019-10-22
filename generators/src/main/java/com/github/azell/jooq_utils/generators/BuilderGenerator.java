93f94d236d0c860fe66959e46339ea78505c30c4
Adam Zell <zellster@gmail.com>
2019-10-21 21:20:34 -0700

    jOOQ integration.

--- generators/src/main/java/com/github/azell/jooq_utils/generators/BuilderGenerator.java
+++ generators/src/main/java/com/github/azell/jooq_utils/generators/BuilderGenerator.java
@@ -1,5 +1,12 @@
 package com.github.azell.jooq_utils.generators;
 
-public class BuilderGenerator {
-  //
+import org.jooq.codegen.JavaGenerator;
+import org.jooq.codegen.JavaWriter;
+import org.jooq.meta.Definition;
+
+public class BuilderGenerator extends JavaGenerator {
+  @Override
+  protected void generatePojoMultiConstructor(Definition tableOrUDT, JavaWriter out) {
+    super.generatePojoMultiConstructor(tableOrUDT, out);
+  }
 }
